3cb2926c941d068f15ea3f12813f5ead34848733
Kennedy Lelei <kennedylelei26@gmail.com>
2023-03-17 11:55:44 -0700

    Print the alphabet in lowercase, followed by a new line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 /**
  * main - Print alphabets in lower case using the putchar
  *
- * Return - 0 (Success)
+ * Return: - 0 (Success)
  */
 
 int main(void)
@@ -12,7 +12,6 @@ int main(void)
 	for (x = 'a'; x <= 'z'; x++)
 	{
 		putchar(x);
-		putchar('\n');
 	}
 
 	return (0);
