b64f0b163f2796c6dd1e460c8f5e4df57386d956
Kennedy Lelei <kennedylelei26@gmail.com>
2023-03-17 12:32:36 -0700

    Print lower case without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Print all lower case alphabets except q & e using putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'e' && a != 'q')
+		{
+			putchar(a);
+		}
+		a++;
+	}
+
+	putchar('\n');
+
+	return(0);
+}
