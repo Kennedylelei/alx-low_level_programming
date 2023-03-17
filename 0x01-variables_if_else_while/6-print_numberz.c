5296a84d9dc84ed384ddccea4779ac8bf9a19076
Kennedy Lelei <kennedylelei26@gmail.com>
2023-03-17 14:15:21 -0700

    Print digits using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Prints all single digit numbers of base 10 starting from 0,
- * followed by a new line
+ * followed by a new line, using putchar
  *
  * Return: 0 (Success)
  */
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 48; i <= 58; i++)
 	{
 		putchar(i);
 	}
