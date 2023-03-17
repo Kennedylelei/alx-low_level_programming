076083ce50f456674fffdb948a8ca86dc33ee62a
Kennedy Lelei <kennedylelei26@gmail.com>
2023-03-17 14:28:38 -0700

    prints alphabets in reverse using putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse, followed by a new line,
+ * using putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
